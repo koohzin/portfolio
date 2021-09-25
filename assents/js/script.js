c489bc13461a5edd1028d1a6feb16e5c69d66d1e
koohzin <joao.koguishi@gmail.com>
2021-09-25 01:56:44 -0300

    icon

--- assents/js/script.js
+++ assents/js/script.js
@@ -3,22 +3,63 @@ function vinicio(){
     document.getElementById('sobre1').style.display = 'none';
     document.getElementById('contato1').style.display = 'none';
     document.getElementById('habilidades1').style.display = 'none';
+        if(document.getElementById('apresentacao1').style.display == 'flex'){
+        colorapresentacao.style.color = '#F4827A'
+        colorsobre.style.color = 'black'
+        colorhab.style.color = 'black'
+        colorcontato.style.color = 'black'
+}
 }
 function vsobre(){
     document.getElementById('apresentacao1').style.display = 'none';
     document.getElementById('sobre1').style.display = 'flex';
     document.getElementById('contato1').style.display = 'none';
     document.getElementById('habilidades1').style.display = 'none';
+
+        if(document.getElementById('sobre1').style.display == 'flex'){
+
+        colorapresentacao.style.color = 'black'
+        colorsobre.style.color = '#F4827A'
+        colorhab.style.color = 'black'
+        colorcontato.style.color = 'black'
+}
 }
 function vcontato(){
     document.getElementById('apresentacao1').style.display = 'none';
     document.getElementById('sobre1').style.display = 'none';
     document.getElementById('contato1').style.display = 'flex';
     document.getElementById('habilidades1').style.display = 'none';
+
+        if(document.getElementById('contato1').style.display == 'flex'){
+
+        colorapresentacao.style.color = 'black'
+        colorsobre.style.color = 'black'
+        colorhab.style.color = 'black'
+        colorcontato.style.color = '#F4827A'
+}
 }
 function vhabilidade(){
     document.getElementById('apresentacao1').style.display = 'none';
     document.getElementById('sobre1').style.display = 'none';
     document.getElementById('contato1').style.display = 'none';
     document.getElementById('habilidades1').style.display = 'flex';
+
+        if(document.getElementById('habilidades1').style.display == 'flex'){
+
+        colorapresentacao.style.color = 'black'
+        colorsobre.style.color = 'black'
+        colorhab.style.color = '#F4827A'
+        colorcontato.style.color = 'black'   
+        }
 }
+
+var iconinicio = document.getElementById('apresentacao1');
+var iconsobre = document.getElementById('sobre1');
+var iconhab = document.getElementById('habilidades1');
+var iconcontato = document.getElementById('contato1');
+
+var colorapresentacao = document.getElementById('iconinicio');
+var colorsobre = document.getElementById('iconsobre');
+var colorhab = document.getElementById('iconhabilidade');
+var colorcontato = document.getElementById('iconcontato');
+
